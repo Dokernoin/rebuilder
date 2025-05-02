d2d49d2a4d051514422b55871ab7d7625d1a9ea8
Dokernoin <cs@false9.co.kr>
2025-05-03 03:14:42 +0900

    최신글 탭 기능 카테고리 조회 구문 추가 (2.2)

--- _빌더설치/rb/rb.lib/ajax.res.php
+++ _빌더설치/rb/rb.lib/ajax.res.php
@@ -33,6 +33,38 @@ $is_shop = isset($_POST['is_shop']) ? $_POST['is_shop'] : '';
     
 <?php } ?>
 
+<?php if(isset($mod_type) && $mod_type == "ca_name_tab") {
+
+    $md_bo_table = isset($_POST['md_bo_table']) ? $_POST['md_bo_table'] : '';
+
+    if(isset($md_bo_table) && $md_bo_table) {
+
+        // 해당게시판의 카테고리 조회
+        $res_ca = sql_fetch (" select bo_category_list from {$g5['board_table']} where bo_table = '{$md_bo_table}' and bo_use_category = '1' ");
+        $cat = isset($res_ca['bo_category_list']) ? $res_ca['bo_category_list'] : '';
+        $cat_opt = explode("|", $cat);
+
+    }
+?>
+    <?php if(isset($md_bo_table) && $md_bo_table) { ?>
+    <?php if(isset($cat) && $cat) { ?>
+    <ul class="mt-5 selected_tab selected_select">
+        <select class="select w100" name="md_sca_tab" id="tab_sca">
+
+                <option value="">카테고리를 선택하세요.</option>
+                <?php foreach($cat_opt as $option): ?>
+                <option value="<?php echo $option; ?>"><?php echo $option; ?></option>
+                <?php endforeach; ?>
+
+                <option value="">전체</option>
+
+        </select>
+    </ul>
+    <?php } ?>
+    <?php } ?>
+
+<?php } ?>
+
 
 <?php 
     if(isset($mod_type) && $mod_type == "mod_order") { 
