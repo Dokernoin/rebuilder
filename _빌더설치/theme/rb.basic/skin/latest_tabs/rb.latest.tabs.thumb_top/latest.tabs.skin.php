156e688b1d6e831f95fc2d19308fa7b7acc176ef
Dokernoin <cs@false9.co.kr>
2025-05-05 02:18:41 +0900

    최신글 탭 전용스킨 추가 및 CSS수정 (2.2)

--- _빌더설치/theme/rb.basic/skin/latest_tabs/rb.latest.tabs.thumb_top/latest.tabs.skin.php
+++ _빌더설치/theme/rb.basic/skin/latest_tabs/rb.latest.tabs.thumb_top/latest.tabs.skin.php
@@ -10,42 +10,9 @@ if(isset($rb_skin['md_title']) && $rb_skin['md_title']) {
     $bo_subjects = $rb_skin['md_title'];
 }
 
-/*
-모듈설정 연동 변수
-$rb_skin['md_id'] 설정ID
-$rb_skin['md_layout'] 레이아웃 섹션ID
-$rb_skin['md_layout_name'] 레이아웃 스킨명
-$rb_skin['md_theme'] 테마명
-$rb_skin['md_title'] 타이틀(제목)
-$rb_skin['md_bo_table'] 게시판ID
-$rb_skin['md_skin'] 스킨명
-$rb_skin['md_cnt'] 출력갯수
-$rb_skin['md_col'] 행갯수
-$rb_skin['md_row'] 열갯수
-$rb_skin['md_col_mo'] 행갯수(모바일)
-$rb_skin['md_row_mo'] 열갯수(모바일)
-$rb_skin['md_gap'] 게시물 간격(여백)
-$rb_skin['md_gap_mo'] 모바일 게시물 간격(여백)
-$rb_skin['md_width'] 가로사이즈
-$rb_skin['md_height'] 세로사이즈
-$rb_skin['md_auto_time'] 자동롤링 시간
-$rb_skin['md_thumb_is'] 썸네일 출력여부(1,0)
-$rb_skin['md_nick_is'] 닉네임 출력여부(1,0)
-$rb_skin['md_date_is'] 작성일 출력여부(1,0)
-$rb_skin['md_content_is'] 본문내용 출력여부(1,0)
-$rb_skin['md_icon_is'] 아이콘 출력여부(1,0)
-$rb_skin['md_comment_is'] 댓글수 출력여부(1,0)
-$rb_skin['md_swiper_is'] 스와이프 여부(1,0)
-$rb_skin['md_auto_is'] 자동롤링 여부(1,0)
-$rb_skin['md_tab_skin'] 탭 스킨명
-$rb_skin['md_tab_list'] 탭 설정
-*/
-
 ?>
 
 <link rel="stylesheet" href="<?php echo $latest_skin_url ?>/style.css?ver=<?php echo G5_SERVER_TIME ?>">
-
-
 
 <div class="bbs_main">
     <!-- 제목 영역 -->
@@ -104,7 +71,7 @@ $rb_skin['md_tab_list'] 탭 설정
         $cate = $tab['sca'];
       ?>
 
-            <div class="latest-tab-content<?php echo $i == 0 ? ' active' : ''; ?>" id="tab-<?php echo $rb_skin['md_id'].'-'.$i; ?>">
+            <div class="latest-tab-content-thumb-top<?php echo $i == 0 ? ' active' : ''; ?>" id="tab-<?php echo $rb_skin['md_id'].'-'.$i; ?>">
 
                 <?php
         if(isset($cate) && $cate) {
@@ -119,22 +86,26 @@ $rb_skin['md_tab_list'] 탭 설정
 
                     <div class="rb_swiper_inner">
                         <div class="rb-swiper-wrapper swiper-wrapper">
+
                             <?php foreach ($list as $row): ?>
                             <?php
-                  $thumb = get_list_thumbnail($bo_table, $row['wr_id'], $thumb_width, $thumb_height, false, true);
-                  $img = ($thumb['src'] && !strstr($row['wr_option'], 'secret')) ? $thumb['src'] : (strstr($row['wr_option'], 'secret') ? G5_THEME_URL.'/rb.img/sec_image.png' : G5_THEME_URL.'/rb.img/no_image.png');
-                  $thumb_alt = $thumb['alt'] ?: '이미지';
-                  $wr_href = get_pretty_url($bo_table, $row['wr_id']);
-                  $wr_content = strip_tags($row['wr_content']);
-                  $is_secret = strstr($row['wr_option'], 'secret');
-                ?>
+                              $thumb = get_list_thumbnail($bo_table, $row['wr_id'], $thumb_width, $thumb_height, false, true);
+                              $img = ($thumb['src'] && !strstr($row['wr_option'], 'secret')) ? $thumb['src'] : (strstr($row['wr_option'], 'secret') ? G5_THEME_URL.'/rb.img/sec_image.png' : G5_THEME_URL.'/rb.img/no_image.png');
+                              $thumb_alt = $thumb['alt'] ?: '이미지';
+                              $wr_href = get_pretty_url($bo_table, $row['wr_id']);
+                              $wr_content = strip_tags($row['wr_content']);
+                              $is_secret = strstr($row['wr_option'], 'secret');
+                            ?>
+
                             <div class="rb_swiper_list">
+
+                                <!-- for { -->
+
                                 <div>
-                                    <?php if ($rb_skin['md_thumb_is']): ?>
+
+                                    <?php if($rb_skin['md_thumb_is'] == 1) { //모듈설정:썸네일 출력여부(1,0)?>
                                     <ul class="bbs_main_wrap_con_ul1">
-                                        <a href="<?php echo $wr_href ?>">
-                                            <img src="<?php echo $img ?>" alt="<?php echo $thumb_alt ?>" class="skin_list_image">
-                                        </a>
+                                        <a href="<?php echo $wr_href ?>"><img src="<?php echo $img ?>" alt="<?php echo $thumb_alt ?>" class="skin_list_image"></a>
 
                                         <?php if($rb_skin['md_icon_is'] == 1) { //모듈설정:아이콘 출력여부(1,0)?>
                                         <div class="icon_abs">
@@ -143,66 +114,90 @@ $rb_skin['md_tab_list'] 탭 설정
                                         </div>
                                         <?php } ?>
                                     </ul>
-                                    <?php endif; ?>
+                                    <?php } ?>
 
-                                    <ul class="bbs_main_wrap_con_ul2" style="<?php echo !$rb_skin['md_thumb_is'] ? 'padding-left:0px !important; min-height:36px;' : '' ?>">
-                                        <?php if($rb_skin['md_subject_is']): ?>
-                                        <li class="bbs_main_wrap_con_subj cut">
-                                            <a href="<?php echo $wr_href ?>" class="font-B"><?php echo $row['subject'] ?></a>
-                                        </li>
-                                        <?php endif; ?>
+                                    <ul class="bbs_main_wrap_con_ul2" <?php if($rb_skin['md_thumb_is'] != 1) { //모듈설정:썸네일 출력하지 않는경우 ?>style="width:100%" <?php } ?>>
 
-                                        <?php if($rb_skin['md_content_is']): ?>
+                                        <?php if($rb_skin['md_subject_is'] == 1) { //모듈설정:제목 출력여부(1,0) ?>
+                                        <li class="bbs_main_wrap_con_subj cut"><a href="<?php echo $wr_href ?>" class="font-B"><?php echo $row['subject'] ?></a></li>
+                                        <?php } ?>
+
+                                        <?php if($rb_skin['md_content_is'] == 1) { //모듈설정:본문 출력여부(1,0)?>
+
+                                        <?php if ($is_secret) { ?>
                                         <li class="bbs_main_wrap_con_cont">
-                                            <?php if ($is_secret): ?>
                                             <a href="<?php echo $wr_href ?>" style="opacity:0.6" class="cut2">작성자 및 관리자 외 열람할 수 없습니다.<br>비밀글 기능으로 보호된 글입니다.</a>
-                                            <?php else: ?>
+                                        </li>
+                                        <?php } else { ?>
+                                        <li class="bbs_main_wrap_con_cont cut2">
                                             <a href="<?php echo $wr_href ?>" class="cut2"><?php echo $wr_content ?></a>
-                                            <?php endif; ?>
                                         </li>
-                                        <?php endif; ?>
+                                        <?php } ?>
 
-                                        <?php if($rb_skin['md_nick_is'] || $rb_skin['md_date_is'] || $rb_skin['md_comment_is']): ?>
+                                        <?php } ?>
+
+                                        <?php if($rb_skin['md_nick_is'] == 1 || $rb_skin['md_date_is'] == 1 || $rb_skin['md_ca_is'] == 1 || $rb_skin['md_comment_is'] == 1) {?>
                                         <li class="bbs_main_wrap_con_info">
-                                            <?php if($rb_skin['md_nick_is']) echo '<span class="prof_tiny_name font-B">'.$row['wr_name'].'</span>'; ?>
-                                            <?php if($rb_skin['md_date_is']) echo passing_time($row['wr_datetime']).'　'; ?>
-                                            <?php if($rb_skin['md_ca_is']) echo $row['ca_name'].'　'; ?>
 
-                                            <?php if($rb_skin['md_comment_is'] && $row['wr_comment']) echo '댓글 '.number_format($row['wr_comment']).'　'; ?>
-                                            조회 <?php echo number_format($row['wr_hit']) ?>
+                                            <?php if($rb_skin['md_nick_is'] == 1) { //모듈설정:작성자 출력여부(1,0)?>
+                                            <span class="font-B"><?php echo $row['wr_name'] ?></span>
+                                            <?php } ?>
+
+                                            <?php if($rb_skin['md_date_is'] == 1) { //모듈설정:작성일 출력여부(1,0)?>
+                                            <?php echo passing_time($row['wr_datetime']) ?>
+                                            <?php } ?>
+
+                                            <?php if($rb_skin['md_ca_is'] == 1 && $row['ca_name']) { //모듈설정:카테고리 출력여부(1,0) || 카테고리 있을때만?>
+                                            <?php echo $row['ca_name'] ?>
+                                            <?php } ?>
+
+                                            <?php if($rb_skin['md_comment_is'] == 1) { //모듈설정:댓글 출력여부(1,0 || 댓글이 0개 이상인 경우)?>
+                                            <?php if($row['comment_cnt']) { ?>
+                                            댓글 <?php echo number_format($row['wr_comment']); ?>
+                                            <?php } ?>
+                                            조회 <?php echo number_format($row['wr_hit']); ?>
+                                            <?php } ?>
+
                                         </li>
-                                        <?php endif; ?>
+                                        <?php } ?>
 
 
                                     </ul>
                                     <div class="cb"></div>
                                 </div>
                             </div>
+                            <!-- } -->
+
                             <?php endforeach; ?>
 
                             <?php if ($list_count == 0): ?>
                             <div class="no_data" style="width:100% !important;">데이터가 없습니다.</div>
                             <?php endif; ?>
+
                         </div>
                     </div>
 
-                    <?php if($rb_skin['md_swiper_is']): ?>
+                    <?php if($rb_skin['md_swiper_is'] == 1) { //모듈설정:스와이프 사용여부(1,0)?>
                     <div class="rb_swiper_paging_btn" style="display:<?php echo (isset($rb_skin['md_title_hide']) && $rb_skin['md_title_hide'] == '1') ? 'none' : 'block'; ?>">
+                        <!-- 좌우 페이징 { -->
                         <button type="button" class="swiper-button-prev rb-swiper-prev">
                             <img src="<?php echo G5_THEME_URL ?>/rb.img/icon/arr_prev.svg">
                         </button>
                         <button type="button" class="swiper-button-next rb-swiper-next">
                             <img src="<?php echo G5_THEME_URL ?>/rb.img/icon/arr_next.svg">
                         </button>
+                        <!-- } -->
                     </div>
-                    <?php endif; ?>
+                    <?php } ?>
 
                 </div>
+
             </div>
             <?php endforeach; ?>
         </div>
     </div>
 </div>
+
 <script>
     $(document).on('click', '.swiper-slide-tab-<?php echo $rb_skin['md_id']; ?> a', function(e) {
         e.preventDefault();
@@ -217,7 +212,7 @@ $rb_skin['md_tab_list'] 탭 설정
         $this.addClass('active');
 
         // 탭 콘텐츠 전환
-        $wrap.find('.latest-tab-content').removeClass('active');
+        $wrap.find('.latest-tab-content-thumb-top').removeClass('active');
         const $targetTab = $wrap.find('#' + targetId).addClass('active');
 
         // 탭 콘텐츠가 보이도록 된 후 슬라이더 초기화
