5b03ef31ccddc6d2585a282a2da5509ae452369f
Dokernoin <cs@false9.co.kr>
2025-05-06 05:15:56 +0900

    빌더버전 2.2.0.1 변경 (2.2.0.1)

--- _빌더설치/extend/rb_core.extend.php
+++ _빌더설치/extend/rb_core.extend.php
@@ -2,7 +2,7 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 //ini_set("display_errors", 1); // 디버깅
-define('RB_VER',  '2.2'); // 버전
+define('RB_VER',  '2.2.0.1'); // 버전
 
 
 /*********************************************/
