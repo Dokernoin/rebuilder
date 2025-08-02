b080658d91fc19ad260627d79b74d5a19d5308e4
Dokernoin <cs@false9.co.kr>
2025-08-02 20:30:44 +0900

    마켓 > 서브사이드 영역 제외 페이지 추가 (2.2.2)

--- _빌더설치/theme/rb.basic/shop/shop.head.php
+++ _빌더설치/theme/rb.basic/shop/shop.head.php
@@ -31,7 +31,16 @@ if(defined('_INDEX_')) { // index에서만 실행
 
 // 사이드영역 숨김처리
 $current_page = basename($_SERVER['PHP_SELF']);
-if ($current_page === 'mypage.php' || $current_page === 'partner.php') {
+if (
+    $current_page === 'mypage.php' ||
+    $current_page === 'partner.php' ||
+    $current_page === 'cart.php' ||
+    $current_page === 'orderform.php' ||
+    $current_page === 'orderinquiry.php' ||
+    $current_page === 'orderinquiryview.php' ||
+    $current_page === 'couponzone.php' ||
+    $current_page === 'wishlist.php'
+) {
     echo '<style>#rb_sidemenu_shop {display:none;} #rb_sidemenu_float_shop {width:100% !important; float:none !important;}</style>';
 }
 
